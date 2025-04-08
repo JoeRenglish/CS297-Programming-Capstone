7e3f33aab97e1c08685dabcfc3348684dddc70eb
JoeRenglish <englishjr@my.lanecc.edu>
2025-04-08 15:49:25 -0700

    started calendar for homepage

---
+++ NewMvcApp/NewMvcApp/wwwroot/js/calendar.js
@@ -0,0 +1,29 @@
+const calendarDates = document.querySelector('.calendar-dates');
+const monthYear = document.getElementById('month-year');
+const prevMonthBtn = document.getElementById('prev-month');
+const nextMonthBtn = document.getElementById('next-month');
+
+let currentDate = new Date();
+let currentMonth = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+];
+
+function renderCalendar(month, year) {
+    calendarDates.innerHTML = '';
+    monthYear.textContent = `${months[month]} ${year}`;
+    
+    const firstDay = new Date(year, month, 1).getDay();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    
+    for (let i = 1; i <= daysInMonth; i++) {
+        const day = document.createElement('div');
+        day.textContent = i;
+        calendarDates.appendChild(day);
+        
+    }
+    
+}
+renderCalendar(currentMonth, currentYear);
